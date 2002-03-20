a334ef3d3a50c442294ddf0e3d73d8237afb5bd2
pphaneuf <pphaneuf@ed29a4e4-1508-0410-9ff7-8f0dc4a294a8>
2002-03-20 18:03:26 +0000

    More work done on test007, it is almost finished (but not yet).

--- trunk/xplc/tests/test007/test.cpp
+++ trunk/xplc/tests/test007/test.cpp
@@ -20,7 +20,9 @@
  */
 
 #include "../test.h"
-#include <xplc/IMoniker.h>
+#include <xplc/utils.h>
+#include <xplc/IStaticServiceHandler.h>
+#include <xplc/IMonikerService.h>
 
 /*
  * test007
@@ -30,14 +32,44 @@
 
 void test() {
   IServiceManager* servmgr;
-  IMoniker* monikers;
+  IStaticServiceHandler* handler;
+  IMonikerService* monikers;
+  IObject* test;
   IObject* obj;
 
   servmgr = XPLC::getServiceManager();
   ASSERT(servmgr != 0, "could not obtain service manager");
 
+  obj = servmgr->getObject(XPLC::staticServiceHandler);
+  ASSERT(obj != 0, "could not obtain static service handler");
+
+  handler = mutateInterface<IStaticServiceHandler>(obj);
+  ASSERT(handler != 0, "static service handler does not have the IStaticServiceHandler interface");
+
+  test = new TestObject;
+  ASSERT(test != 0, "could not create TestObject");
+
+  handler->addObject(TestObject_CID, test);
+  VERIFY(servmgr->getObject(TestObject_CID) == test, "adding the test object did not work");
+  VERIFY(test->release() == 1, "incorrect refcount on test object");
+
+  VERIFY(handler->release() == 2, "incorrect refcount on static service handler");
+
   obj = servmgr->getObject(XPLC::monikers);
   ASSERT(obj != 0, "could not obtain moniker component");
+  
+  monikers = mutateInterface<IMonikerService>(obj);
+  ASSERT(monikers != 0, "moniker service does not have the IMoniker interface");
 
+  monikers->registerObject("testobject", TestObject_CID);
+
+  obj = monikers->resolve("testobject");
+  ASSERT(obj != 0, "resolving the test object returned nothing");
+  ASSERT(obj == test, "the testobject moniker resolved to something else than the test object");
+
+  VERIFY(obj->release() == 1, "refcount is wrong on the test object");
+
+  servmgr->shutdown();
+  VERIFY(servmgr->release() == 0, "service manager has non-zero refcount after shutdown/release");
 }
 
