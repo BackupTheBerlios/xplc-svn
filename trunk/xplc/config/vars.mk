5aac211f7f39d2e59b5438175f3307602481209d
pphaneuf <pphaneuf@ed29a4e4-1508-0410-9ff7-8f0dc4a294a8>
2002-03-29 21:26:44 +0000

    Moved most of the variable-setting login from config.mk.in to config/vars.mk, cleaned up some of the code to make it clearer, removed old debugging code (NIT).

--- trunk/xplc/config/vars.mk
+++ trunk/xplc/config/vars.mk
@@ -1,5 +1,6 @@
 # XPLC - Cross-Platform Lightweight Components
 # Copyright (C) 2000-2002, Pierre Phaneuf
+# Copyright (C) 2002, Net Integration Technologies, Inc.
 #
 # This library is free software; you can redistribute it and/or modify
 # it under the terms of the GNU Library General Public License as
@@ -23,9 +24,44 @@ REALCLEAN+=ChangeLog
 
 SIMPLETARGETS+=dist ChangeLog clean dustclean realclean distclean
 
-# the 'S' option to 'ar' seems to cause problem on Solaris?
+# The 'S' option to 'ar' seems to cause problem on Solaris?
 #ARFLAGS=rcS
 ARFLAGS=rc
 
 RANLIB=ranlib
+
+CXXFLAGS+=-pipe -Iinclude
+
+ifneq ("$(filter-out no,$(enable_debug))", "")
+DEBUG:=$(enable_debug)
+endif
+
+ifdef DEBUG
+CXXFLAGS+=-ggdb -DDEBUG
+ifneq ("$(DEBUG)", "yes")
+CXXFLAGS+=-DDEBUG_$(DEBUG)
+endif
+else
+CXXFLAGS+=-DNDEBUG
+endif
+
+ifeq ("$(enable_fatal_warnings)", "yes")
+CXXFLAGS+=-Werror
+endif
+
+ifneq ("$(enable_optimization)", "no")
+CXXFLAGS+=-O2
+endif
+
+ifneq ("$(enable_warnings)", "no")
+CXXFLAGS+=-pedantic -Wall -Wold-style-cast -Woverloaded-virtual
+endif
+
+ifneq ("$(enable_rtti)", "yes")
+CXXFLAGS+=-fno-rtti
+endif
+
+ifneq ("$(enable_exceptions)", "yes")
+CXXFLAGS+=-fno-exceptions
+endif
 
