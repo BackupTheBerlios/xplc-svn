dac6ca10463f35eef21f12fb59c362e01f482909
pphaneuf <pphaneuf@ed29a4e4-1508-0410-9ff7-8f0dc4a294a8>
2004-03-17 21:27:37 +0000

    Forgot to update the copyrights.

--- trunk/xplc/xplc/moduleloader.cpp
+++ trunk/xplc/xplc/moduleloader.cpp
@@ -1,7 +1,7 @@
 /* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
  *
  * XPLC - Cross-Platform Lightweight Components
- * Copyright (C) 2002, Net Integration Technologies, Inc.
+ * Copyright (C) 2002-2004, Net Integration Technologies, Inc.
  * Copyright (C) 2002-2004, Pierre Phaneuf
  * Copyright (C) 2002-2004, Stéphane Lajoie
  *
