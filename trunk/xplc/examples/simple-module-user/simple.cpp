7e80bde0037a49f2d7d93c128c5e5a2664d06501
pphaneuf <pphaneuf@ed29a4e4-1508-0410-9ff7-8f0dc4a294a8>
2004-07-01 20:35:52 +0000

    Putting the example code into the public domain.

--- trunk/xplc/examples/simple-module-user/simple.cpp
+++ trunk/xplc/examples/simple-module-user/simple.cpp
@@ -1,3 +1,9 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*-
+ *
+ * This file is example code for XPLC (http://xplc.sourceforge.net/),
+ * and is put into the public domain.
+ */
+
 #include <stdio.h>
 #include <xplc/xplc.h>
 #include "IExample.h"
