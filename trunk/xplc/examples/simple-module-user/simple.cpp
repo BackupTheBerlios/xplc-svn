8b460272d33c6209e856960cd26d4587e6b15f07
pphaneuf <pphaneuf@ed29a4e4-1508-0410-9ff7-8f0dc4a294a8>
2004-07-01 20:49:34 +0000

    Added a comment beside the XPLC helper object instance.

--- trunk/xplc/examples/simple-module-user/simple.cpp
+++ trunk/xplc/examples/simple-module-user/simple.cpp
@@ -9,6 +9,22 @@
 #include "IExample.h"
 #include "simple.h"
 
+/*
+ * This is an helper class from the C++ binding. While it is not XPLC
+ * itself (despite the name), it makes XPLC itself easier to use in
+ * C++.
+ *
+ * It should live at least as long as you intend to use XPLC, as it
+ * keeps it alive.
+ *
+ * Although this wastes a tiny bit of space, it is allowed to have
+ * more than one XPLC helper object in a single process, since XPLC
+ * uses reference counting. This is useful for libraries, which can
+ * create their own XPLC helper object, and if the main application
+ * also uses XPLC, or if another library used by the application uses
+ * XPLC (even if the application doesn't!), they will be able to
+ * cooperate through XPLC.
+ */
 XPLC xplc;
 
 int main() {
